5ed590142cf44a461c27b07c53d63f67f05f07be
RadduGit <radugabriel919@gmail.com>
2017-04-24 16:35:23 +0300

    Derived from entity

--- rock.cpp
+++ rock.cpp
@@ -1,32 +1,28 @@
 #include "rock.h"
-Rock :: Rock(float dim1, float dim2, float p1, float p2, float coef, char tpath[]) : speed(coef), rx(p1), ry(p2), dimx(dim1), dimy(dim2), rock(sf::Vector2f(dim1, dim2))
-{
+#include <iostream>
+using namespace std;
+Rock :: Rock(float dim1, float dim2, float p1, float p2, float coef, char tpath[]) : Entity(dim1, dim2, p1, p2, coef, tpath)
+{ 
 	initx = p1;
 	inity = p2;
-	rtext.loadFromFile(tpath);
-	rock.setTexture(&rtext);
-	rock.setPosition(rx, ry);
 }
 
 void Rock :: Descend()
 {
-	rock.move(0.0f, speed);
-	ry += speed;
-}
+	shape.move(0.0f, speed);
+	py += speed;
 
-void Rock :: Appear(sf::RenderWindow& window)
-{
-	window.draw(rock);
 }
 
 bool Rock :: InBounds()
-{
-	return ry <= 768.0f;
+{   
+	cout << "InBounds\n" << py << "\n";
+	return py <= 768.0f;
 }
 
 void Rock :: Reestablish()
 {
-	rx = initx;
-	ry = inity;
-	rock.setPosition(initx, inity);
+	px = initx;
+	py = inity;
+	shape.setPosition(initx, inity);
 }
