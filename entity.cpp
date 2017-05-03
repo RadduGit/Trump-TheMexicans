5c4bbd968eb7132991a873628d5478c29dc847a2
BogdanAngh <bogdananghelache1097@gmail.com>
2017-05-03 18:38:21 +0300

    Add files via upload

--- entity.cpp
+++ entity.cpp
@@ -1,7 +1,8 @@
 #pragma once
 #include <SFML/Graphics.hpp>
 #include "entity.h"
-Entity::Entity(float dim1, float dim2, float p1, float p2, float coef, char tpath[]) : speed(coef), px(p1), py(p2), dimx(dim1), dimy(dim2), shape(sf::Vector2f(dim1, dim2))
+Entity::Entity(float dim1, float dim2, float p1, float p2, float coef, char tpath[]) : speed(coef), px(p1), py(p2), dimx(dim1),
+																						dimy(dim2), shape(sf::Vector2f(dim1, dim2))
 {
 	text.loadFromFile(tpath);
 	shape.setTexture(&text);
