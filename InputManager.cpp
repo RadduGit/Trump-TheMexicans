a570706ced17e0c07c19be686a92ed41d1d3ff78
BogdanAngh <bogdananghelache1097@gmail.com>
2017-05-11 21:44:18 +0300

    Input Manager cpp

---
+++ InputManager.cpp
@@ -0,0 +1,31 @@
+//#include "InputManager.h"
+//#include "InputManager.h"
+//#include <iostream>
+//#include <SFML\Graphics.hpp>
+////#include "entityManager.h"
+//
+//void InputManager::Verificare(sf::Keyboard::Key key)
+//{
+//	/*
+//	W -> 1
+//	A -> 2
+//	S -> 3
+//	D -> 4
+//	*/
+//	while (1) {
+//		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::W)) {
+//			entityManager.check(1);
+//		}
+//		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::A)) {
+//			entityManager.check(2);
+//		}
+//		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::S)) {
+//			entityManager.check(3);;
+//		}
+//		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::D)) {
+//			entityManager.check(4);
+//		}
+//	}
+//}
+//InputManager InputManager::input(0);
+//
